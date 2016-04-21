bfa18a13a33b9e33d0e2db9d474bd76fb2b5578c
Franton Lin <franton.lin@students.olin.edu>
2016-04-21 15:54:02 -0400

    generating sounds based on bits

--- sonify.py
+++ sonify.py
@@ -2,9 +2,9 @@ import pyaudio
 # import numpy as np
 import math
 
-FREQUENCY = 2000
+FREQUENCY = 1000
 BITRATE = 48000
-DATASIZE = 1024
+DATASIZE = 4096
 
 PyAudio = pyaudio.PyAudio
 p = PyAudio()
@@ -12,22 +12,27 @@ stream = p.open(format = p.get_format_from_width(1),
                 channels = 1, 
                 rate = BITRATE, 
                 output = True)
+                                
+def generate_tone(frequency, bit):
+    if bit:
+        return ''.join([chr(int(math.sin(x/((BITRATE/frequency)/math.pi))*127+128)) for x in xrange(DATASIZE)])
+    else:
+        return ''.join([chr(128) for x in xrange(DATASIZE)])
 
 print "Type a message"
 while(1):
     text = raw_input(": ")
     message = map(bin, bytearray(text))
-    message = '1111111'.join([item[2:] for item in message])
+    print message
+    message = '11111111'.join([item[2:] for item in message])
+    message = '11111111' + message
+    print message
     # freqs = [int(bit)*FREQUENCY for bit in message]
     # print freqs
-    wavedata = ''.join([generate_tone(FREQUENCY, bit) for bit in message])
+    wavedata = ''.join([generate_tone(FREQUENCY, int(bit)) for bit in message])
+    # print wavedata
     stream.write(wavedata)
-
-
-
-
-def generate_tone(frequency, bit):
-    if bit:
-        return ''.join([chr(int(math.sin(x/((BITRATE/frequency)/math.pi))*127+128)) for x in xrange(BITRATE)])
-    else:
-        return ''.join([chr(128) for x in xrange(BITRATE)])
+    
+stream.stop_stream()
+stream.close()
+p.terminate()
