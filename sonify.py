9b4928a90959e17af4ede8d48cc3701bf43a0922
shanek21 <shane.kelly@students.olin.edu>
2016-04-24 11:23:46 -0400

    Working transmitter

--- sonify.py
+++ sonify.py
@@ -1,11 +1,10 @@
 import pyaudio
-# import numpy as np
 import math
+import time
 
-FREQUENCY = 1000
+FREQUENCY = 2000
 BITRATE = 48000
-DATASIZE = 4096
-
+DATASIZE = 2**14
 PyAudio = pyaudio.PyAudio
 p = PyAudio()
 stream = p.open(format = p.get_format_from_width(1), 
@@ -24,15 +23,32 @@ while(1):
     text = raw_input(": ")
     message = map(bin, bytearray(text))
     print message
-    message = '11111111'.join([item[2:] for item in message])
-    message = '11111111' + message
+    # item = (len(item)-8)*"0" + item
+    message = ''.join([item[2:].zfill(8) for item in message])
+    message = '1' + message
     print message
     # freqs = [int(bit)*FREQUENCY for bit in message]
     # print freqs
     wavedata = ''.join([generate_tone(FREQUENCY, int(bit)) for bit in message])
     # print wavedata
+    # start = time.time()
     stream.write(wavedata)
+    # end = time.time()
+    # print end-start
     
 stream.stop_stream()
 stream.close()
 p.terminate()
+
+
+"""
+
+while (true):
+    currBit = readBit
+
+    if currBit != prevBit
+        times.append(time.time())
+
+    prevBit = currBit
+
+"""
